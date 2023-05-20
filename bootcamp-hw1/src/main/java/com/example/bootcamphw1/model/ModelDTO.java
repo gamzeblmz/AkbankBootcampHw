c06fb4e93037b31ed12a08d1be68fa1042cbb1a0
GamzeBilmez <C9019274@arcelik.com>
2023-05-20 13:29:31 +0300

    dto lar guncellendi.

--- bootcamp-hw1/src/main/java/com/example/bootcamphw1/model/ModelDTO.java
+++ bootcamp-hw1/src/main/java/com/example/bootcamphw1/model/ModelDTO.java
@@ -1,13 +1,16 @@
 package com.example.bootcamphw1.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class ModelDTO {
     private Long id;
     private float price;
+
     private float meters;
     private int totalNumberOfHalls;
     private int totalNumberOfRooms;
