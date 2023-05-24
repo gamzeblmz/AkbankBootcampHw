10dfebd244e4770d48419b5c355ac27ce199c2aa
GamzeBilmez <C9019274@arcelik.com>
2023-05-24 14:28:58 +0300

    user endpoint geliştirmesi

--- bootcamp-hw2/src/main/java/com/example/bootcamphw2/entitiy/User.java
+++ bootcamp-hw2/src/main/java/com/example/bootcamphw2/entitiy/User.java
@@ -3,6 +3,8 @@ package com.example.bootcamphw2.entitiy;
 import com.example.bootcamphw2.enums.UserType;
 import com.example.bootcamphw2.general.BaseEntitiy;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,10 +17,20 @@ public class User extends BaseEntitiy {
     @GeneratedValue(generator = "User", strategy = GenerationType.SEQUENCE)
     @SequenceGenerator(name = "User", sequenceName = "USER_ID_SEQ")
     private Long id;
+    @Column(name = "NAME", length = 100, nullable = false)
     private String name;
+    @Column(name = "SURNAME", length = 100, nullable = false)
     private String surName;
+    @Column(name = "USERNAME", length = 50, nullable = false, unique = true)
     private String userName;
+    @Email
+    @Column(name = "EMAIL", length = 50)
     private String email;
+
+    @NotNull
+    @Column(name = "PASSWORD", length = 400, nullable = false)
+    private String password;
+
     private String phoneNo;
     private UserType userType;
 
